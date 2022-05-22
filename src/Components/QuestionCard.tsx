37f9e440f7e7e096306c9ede72e11ce70108416f
Serena Graham <serenaelizabeth@hotmail.co.uk>
2022-05-22 19:48:50 +0100

    replace dangerously set HTML

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -13,17 +13,22 @@ type Props = {
 }
 
 const QuestionCard: React.FC<Props> = ({ question, answers, handleClick, userAnswer, questionNum, totalQuestions }) => {
+
+    //API repsonse contains &quot characters, this regex replaces them
+    question = question.replace(/&quot;/g, '"').replace(/(&rsquo;|&#039;)/g, "'");
+    answers = answers.map((answer) => answer.replace(/&quot;/g, '"').replace(/&rsquo;&#039/g, "'"))
+
     return (
         <Wrapper>
             <p className='number'>
                 Question: {questionNum} / {totalQuestions}
             </p>
-            <p dangerouslySetInnerHTML={{ __html: question }} />
+            <p> {question}</p>
             <div>
                 {answers.map((answer) => (
                     <ButtonWrapper isCorrect={userAnswer?.correctAnswer === answer} userClicked={userAnswer?.answer === answer} key={answer}>
                         <button disabled={userAnswer ? true : false} onClick={handleClick} value={answer}>
-                            <span dangerouslySetInnerHTML={{ __html: answer }} />
+                            <span>{answer}</span>
                         </button>
                     </ButtonWrapper>
 
